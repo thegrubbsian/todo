29983c63c0bd8ecffd1ec90ef000c20351cdb28c
JC Grubbs <jc.grubbs@devmynd.com>
2014-08-06 23:15:57 -0500

    shouldn't have to do that

--- mix.exs
+++ mix.exs
@@ -12,19 +12,16 @@ defmodule Todo.Mixfile do
   def application do
     [
       mod: { Todo, [] },
-      applications: [:poolboy, :phoenix, :postgrex, :decimal, :ecto]
+      applications: [:phoenix, :postgrex, :ecto]
     ]
   end
 
   defp deps do
     [
-      {:poolboy, "~> 1.2.1"},
-      {:decimal, "~> 0.2.3", optional: true},
       {:postgrex, "~> 0.5.4"},
       {:ecto, "~> 0.2.3"},
       {:jsex, "~> 2.0.0"},
       {:cowboy, "~> 1.0.0"},
-      {:ranch, "~> 1.0.0"},
       {:phoenix, github: "phoenixframework/phoenix"}
     ]
   end
