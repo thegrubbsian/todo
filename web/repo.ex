3917d8eec3674e51b8141f6d94355ba2a1c340bb
JC Grubbs <jc.grubbs@devmynd.com>
2014-08-06 21:12:34 -0500

    get db connection based on envar

--- web/repo.ex
+++ web/repo.ex
@@ -2,7 +2,7 @@ defmodule Repo do
   use Ecto.Repo, adapter: Ecto.Adapters.Postgres
 
   def conf do
-    parse_url "ecto://postgres:@localhost/elixir_todo"
+    parse_url System.get_env("DATABASE_URL") || "ecto://postgres:@localhost/elixir_todo"
   end
 
   def priv do
