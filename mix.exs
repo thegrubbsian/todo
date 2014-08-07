7654bfacb0178c7969397a3e103494684d397801
JC Grubbs <jc.grubbs@devmynd.com>
2014-08-06 23:37:36 -0500

    deps'

--- mix.exs
+++ mix.exs
@@ -19,11 +19,12 @@ defmodule Todo.Mixfile do
   defp deps do
     [
       {:poolboy, "~> 1.2.1"},
-      {:decimal, "~> 0.2.3"},
+      {:decimal, "~> 0.2.4"},
       {:postgrex, "~> 0.5.4"},
       {:ecto, "~> 0.2.3"},
       {:jsex, "~> 2.0.0"},
       {:cowboy, "~> 1.0.0"},
+      {:ranch, "~> 1.0.0"},
       {:phoenix, github: "phoenixframework/phoenix"}
     ]
   end
