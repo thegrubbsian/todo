48d6e879f92ce7c0d365bc7f15c6e74332b29549
JC Grubbs <jc.grubbs@devmynd.com>
2014-08-06 23:28:53 -0500

    apps

--- lib/tasks.ex
+++ lib/tasks.ex
@@ -1,8 +1,6 @@
 defmodule Todo do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/Application.Behaviour.html
-  # for more information on OTP Applications
   def start(_type, _args) do
     Todo.Supervisor.start_link
   end
